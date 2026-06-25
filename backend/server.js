6b0ce69cd2abff5f9d6dd9951e4fb97cb2979a99
rekireddysushruthareddy999 <rekireddysushruthareddy999@gmail.com>
2026-06-25 06:57:52 +0530

    Fix profileRoutes filename case

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-import profileRoutes from './routes/profileRoutes.js';
+import profileRoutes from './routes/profileRoute.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB();
